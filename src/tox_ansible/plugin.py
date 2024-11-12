aa41d628ecf826896d09c094e3d71e848e6e4430
abhikdps <abanand@redhat.com>
2024-11-12 14:57:44 +0530

    Create test_all script

--- src/tox_ansible/plugin.py
+++ src/tox_ansible/plugin.py
@@ -42,7 +42,6 @@ ALLOWED_EXTERNALS = [
     "ls",
 ]
 ENV_LIST = """
-{integration, sanity, unit}-py3.9-{2.15}
 {integration, sanity, unit}-py3.10-{2.15, 2.16, 2.17}
 {integration, sanity, unit}-py3.11-{2.15, 2.16, 2.17, 2.18, milestone, devel}
 {integration, sanity, unit}-py3.12-{2.16, 2.17, 2.18, milestone, devel}
